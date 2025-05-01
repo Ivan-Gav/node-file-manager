882e2387b18557105dd3e1f38ad70f43b179e514
Ivan Gavrilin <ivan.gavrilin@inbox.ru>
2025-05-01 21:33:09 +0300

    feat: implement exit message

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import { stdin, stdout } from "node:process";
+import readline from "node:readline/promises";
+
 import { getUserName } from "#utils";
 
 let username;
@@ -6,4 +9,41 @@ try {
   console.log(`Welcome to the File Manager, ${username}!`);
 } catch (error) {
   console.error("Invalid input");
+  // throw new Error("Invalid input")
+}
+
+const rl = readline.createInterface({ input: stdin, output: stdout });
+
+const onExit = () => {
+  console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
+  process.exit(0);
+};
+
+process.on("SIGINT", onExit);
+
+const commands = {
+  ".exit": onExit,
+  // Список доступных команд
+};
+
+try {
+  while (true) {
+    const line = await rl.question("> ");
+    const input = line.trim();
+
+    const command = commands[input];
+
+    if (command) {
+      command();
+    } else {
+      console.error(`Invalid input: "${input}"`);
+    }
+  }
+} catch (err) {
+  if (err?.code === "ABORT_ERR") {
+    onExit(); // Ctrl+C
+  } else {
+    console.error("Invalid input:", err);
+    process.exit(1);
+  }
 }
